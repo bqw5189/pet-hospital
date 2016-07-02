2e20c9598c4a9388e4a9613641f2f6239e7fed30
bailj <bailijiangzz@163.com>
2016-07-02 19:29:23 +0800

    add item type entity and service

---
+++ fionapet-business/fionapet-business-service-provider/src/main/java/com/fionapet/business/repository/ItemTypeDao.java
@@ -0,0 +1,10 @@
+package com.fionapet.business.repository;
+
+import com.fionapet.business.entity.ItemType;
+
+/**
+ * 商品和服务类型
+ * @author baiqw
+ **/
+public interface ItemTypeDao extends DaoBase<ItemType> {
+}
