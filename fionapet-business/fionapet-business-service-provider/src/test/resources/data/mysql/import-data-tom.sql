51e2e0e6ac3c4ff95ace5bf7ebccf70d8dc12d14
tom <tom@daoshenggroup.com>
2016-07-02 12:11:53 +0800

    add tom sql

---
+++ fionapet-business/fionapet-business-service-provider/src/test/resources/data/mysql/import-data-tom.sql
@@ -0,0 +1,7 @@
+-- t_pet_race --
+insert into t_pet_race (uuid, name, create_user_id, update_user_id) values('1aa38af8-efd0-4fbb-8c78-d12d17cc9605','小型犬','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58');
+insert into t_pet_race (uuid, name, create_user_id, update_user_id) values('34ff5c2e-6625-4d6d-ad39-d1745755b3a8','中型犬','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58');
+insert into t_pet_race (uuid, name, create_user_id, update_user_id) values('aa00b4e5-4668-494a-9799-b2ec542a7205','大型犬','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58');
+insert into t_pet_race (uuid, name, create_user_id, update_user_id) values('326209c2-59aa-474a-b714-49f3cba8675f','猫','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58');
+insert into t_pet_race (uuid, name, create_user_id, update_user_id) values('41fa0032-2400-4ffc-8e16-ed4ec2e6d1c5','兔','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58');
+-- t_pet_race --
