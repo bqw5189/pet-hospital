0db0f3a5b3f853ac9f2e654633f45c01edcfa052
tom <tom@daoshenggroup.com>
2016-07-02 12:51:42 +0800

    add dealer data

--- fionapet-business/fionapet-business-service-provider/src/test/resources/data/mysql/import-data-tom.sql
+++ fionapet-business/fionapet-business-service-provider/src/test/resources/data/mysql/import-data-tom.sql
@@ -1,4 +1,7 @@
 -- t_dealer --
 insert into t_dealer (uuid,code,name, create_user_id, update_user_id,company_type) values(uuid(),'JXS0000000001', '北京龙科生物制药有限公司','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58','DEALER');
-
+insert into t_dealer (uuid,code,name, create_user_id, update_user_id,company_type) values(uuid(),'JXS0000000002', '湖南丹维生物科技有限公司','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58','DEALER');
+insert into t_dealer (uuid,code,name, create_user_id, update_user_id,company_type) values(uuid(),'JXS0000000003', '美国瑞普斯生物药品集团有限公司','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58','DEALER');
+insert into t_dealer (uuid,code,name, create_user_id, update_user_id,company_type) values(uuid(),'JXS0000000004', '五湖','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58','DEALER');
+insert into t_dealer (uuid,code,name, create_user_id, update_user_id,company_type) values(uuid(),'JXS0000000005', '宠儿香','fc5db3b3-4063-4a12-a511-880ba19e4b58','fc5db3b3-4063-4a12-a511-880ba19e4b58','DEALER');
 -- t_dealer --
