98a8d4fe98140582cca2930731dfc0a097048bc4
tom <tom@daoshenggroup.com>
2016-10-23 21:52:57 +0800

    modify create persion

--- fionapet-business/fionapet-business-service-provider/src/main/java/com/fionapet/business/service/PersonsServiceImpl.java
+++ fionapet-business/fionapet-business-service-provider/src/main/java/com/fionapet/business/service/PersonsServiceImpl.java
@@ -1,19 +1,12 @@
 package com.fionapet.business.service;
 
-import cn.fiona.pet.account.entity.Role;
 import cn.fiona.pet.account.entity.User;
-import cn.fiona.pet.account.exception.ApiException;
-import cn.fiona.pet.account.facade.LoginVO;
+import cn.fiona.pet.account.service.AccountService;
 import com.fionapet.business.entity.Persons;
-import org.apache.commons.lang3.StringUtils;
+import com.fionapet.business.repository.PersonsDao;
 import org.dubbo.x.repository.DaoBase;
 import org.dubbo.x.service.CURDServiceBase;
-import com.fionapet.business.repository.PersonsDao;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.security.InvalidParameterException;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  *  宠物主人信息
@@ -23,52 +16,38 @@ public class PersonsServiceImpl extends CURDServiceBase<Persons> implements Pers
     @Autowired
     private PersonsDao personsDao;
 
+    @Autowired
+    private AccountService accountService;
+
     @Override
     public DaoBase<Persons> getDao() {
         return personsDao;
     }
 
-    @Override
-    public String login(LoginVO loginVO) throws ApiException {
-        Persons persons = personsDao.findByPersonName(loginVO.getName());
 
+    public AccountService getAccountService() {
+        return accountService;
+    }
 
-
-        if(null != persons){
-            if (StringUtils.isBlank(loginVO.getPassword())){
-                throw new InvalidParameterException("密码不能为空!");
-            }
-
-            if (!loginVO.getPassword().equalsIgnoreCase(persons.getPassword())){
-                throw new InvalidParameterException("密码错误!");
-            }
-
-            return persons.getId();
-        }
-        return null;
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
     }
 
     @Override
-    public boolean validateToken(String token) throws ApiException {
-        Persons persons = personsDao.findOne(token);
-        if (null != persons){
-            return true;
-        }
-        return false;
-    }
+    public Persons createOrUpdte(Persons entity) {
+        Persons persons = super.createOrUpdte(entity);
 
-    @Override
-    public User getByToken(String token) throws ApiException {
-        Persons persons = personsDao.findOne(token);
         User user = new User();
-        user.setId(persons.getId());
         user.setLoginName(persons.getPersonName());
         user.setName(persons.getPersonName());
-        Set<Role> roleSet = new HashSet<Role>();
-        Role role = new Role();
-        role.setCode(persons.getRoleId());
-        roleSet.add(role);
-        user.setRoles(roleSet);
-        return user;
+        user.setPlainPassword(entity.getPassword());
+        user.setId(persons.getId());
+        user.setSalt("6d65d24122c30500");
+
+        accountService.createUser(user);
+
+
+
+        return persons;
     }
 }
